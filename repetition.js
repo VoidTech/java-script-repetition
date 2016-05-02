17e4c9071f632d9e52cdd5f79bbeba0dbe8f9a25
VoidTech <wojciech.wysocki96@gmail.com>
2016-05-02 13:15:11 +0200

    Created repetition.js

--- repetition.js
+++ repetition.js
@@ -1,1 +1,15 @@
 
+// Javascript 
+
+// Array
+
+//#1
+
+var weekdays = ["Måndag","Tisdag","Onsdag","Torsdag","Fredag","Lördag","Söndag"];
+
+// Hämta ur vår array på fäljande sätt
+
+console.log("Exempel 1. Första veckodagen " + weekday[0]);
+// spara i en variebel
+
+var favoriteDay = weekdays[4];
