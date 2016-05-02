a80439ce261bb21fc92ea1eb415c05732db6307c
VoidTech <wojciech.wysocki96@gmail.com>
2016-05-02 13:21:17 +0200

    Created repetition.js

--- repetition.js
+++ repetition.js
@@ -13,3 +13,12 @@ console.log("Exempel 1. Första veckodagen " + weekdays[0]);
 // spara i en variebel
 
 var favoriteDay = weekdays[4];
+
+console.log("Exempel 2. Favorite day" + favoriteDay);
+
+var months = [];
+months[0] = "Januari";
+months[1] = "Februari";
+months[2] = "Mars";
+months[3] = "April";
+months[4] = "May";
