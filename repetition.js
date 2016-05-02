3132ac417833e3f383e0d023d30d47e489131f79
VoidTech <wojciech.wysocki96@gmail.com>
2016-05-02 13:50:08 +0200

    Updated repetition.js

--- repetition.js
+++ repetition.js
@@ -37,4 +37,39 @@ var car = {color: "ful", hasAWheel: true, numberOfSeats: 4};
 // Attribut(key) och ett värde (value)
 // color är attribut/key
 // "ful" är värdet
-console.log();
+
+// Annat exempel
+
+var computer = {color: "grey", brand: "Mac"};
+
+//metod 2
+var student = {};
+student.name = "Anton";
+student.favoriteTeam = "AIK";
+
+//metod 3
+var anotherStudent = new Object{};
+anotherStudent["name"] = "Oskar";
+anotherStudent["favoriteTeam"] = "HIF";
+
+console.log("Den första studenten heter " + student.name);
+console.log("Den andra studenten heter " + anotherStudent["name"]);
+
+// Objekt i arrayer
+
+var students = {student, anotherStudent};
+
+console.log("Första studenten i array: " + students[0].name);
+console.log("Andra studenten i array: " + students[1]["name"]);
+
+// array i array
+
+var favoriteGames = [["Super mario bros 1","Duck Hunt"],["Wii Sports Resort","Zelda twilight princess"]];
+
+console.log("Andra spelet i första listan" + favoriteGames[0][1]);
+
+// Array i objekt
+var superMario64  = {title:"Super Mario 64",countries:["Sverige","USA"],characters:["Mario","Toad","Peach"]};
+console.log("Andra landet är " + superMario64["countries"][1]);
+
+// Objekt i objekt
